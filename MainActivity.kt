71379e7d50094ac0237efb768af96414ba86d493
faizan <user,faizanali@gmail.com>
2019-01-21 21:41:45 +0530

    done...

--- MainActivity.kt
+++ MainActivity.kt
@@ -6,34 +6,44 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.content.pm.ApplicationInfo
 import android.content.pm.PackageInfo
 import android.content.pm.PackageManager
-import android.util.Log
-import java.nio.file.Files.size
-
+import android.widget.ArrayAdapter
+import android.widget.BaseAdapter
+import android.widget.ListAdapter
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var apps: ArrayList<ApplicationInfo>
+    private val apkList = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        b1.setOnClickListener {
+            getApps()
+        }
+
     }
 
-    fun getInstalledApps() {
-        var count: Int = 0
-        var appInfo: ApplicationInfo? = null
-
-        val packList = packageManager.getInstalledPackages(0)
-        for (i in packList.indices) {
+    fun getApps()
+    {
+        val packList = packageManager.getInstalledPackages(0)       //Getting the list of all the installed packages in the system
+        for (i in packList.indices)         // for(i=0;i<=packklist.size();i++), indices is a pre-defined variable to get the index of the collection
+        {
             val packInfo = packList[i]
-            if (packInfo.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0) {
+
+            if (packInfo.applicationInfo.flags and ApplicationInfo.FLAG_SYSTEM == 0)
+            {
                 val appName = packInfo.applicationInfo.loadLabel(packageManager).toString()
-                count++
-                Log.e("App № " + Integer.toString(i), appName)
-                apps.add(packList[i].applicationInfo)
+                apkList.add(appName)
             }
         }
+        /*var myAdapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, apkList)
+        lview.adapter = myAdapter*/
+
+
+        var myAdapter = ArrayAdapter<String>(this, R.layout.apps_installed, apkList)
+        lview.adapter = myAdapter
     }
 }
+
